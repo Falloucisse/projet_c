1332e3bde115eba6ad11e99654c64be0f188dbf3
Falloucisse2-0 <falloucisse2028@icloud.com>
2026-02-17 23:49:57 +0000

    moyenne

--- projet_versionning.c
+++ projet_versionning.c
@@ -1,6 +1,7 @@
 #include<stdio.h>
 int main(){
-    int x,n;
+    int x,n,somme=0,cpt=0,nb_pair=0,nb_impairs=0;
+    float moy;
     do{
     printf("Saisir n entiers :");
     scanf("%d",&n);
@@ -12,5 +13,19 @@ int main(){
         }while(x>0);
         printf("Le nombre que tu as saisi est %d",x);
     }
+      if(x%2==0){
+            somme==somme%x;
+            cpt++;
+        }
+        moy= somme/cpt;
+        printf("la moyenne est %d\n",moy);
+        if(nb_pair!=0){
+             moy=somme/nb_pair;
+            printf("moyenne des nombres paires=%.2f\n",moy);
+        }
+        if(nb_impairs != 0) {
+            moy= somme / nb_impairs;
+            printf("Moyenne des nombres impairs = %.2f\n", moy);
+        }
 return 0;
 }
